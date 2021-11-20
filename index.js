1a78339a9219809306d1fd685ff04efb88181b5f
Shane McDonough <shanemcdman@gmail.com>
2021-11-20 15:51:05 -0800

    Dynamically add and remove tomoe

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-let tomoes = document.querySelectorAll('.tomoe');
-function set_tomoe(){
+let tomoe_div = document.createElement('div');
+tomoe_div.innerHTML = ',';
+tomoe_div.classList.add('tomoe');
+let eye = document.querySelector('.eye');
+
+let tomoes = [];
+function get_tomoe(){
+    tomoes = document.querySelectorAll('.tomoe');
+}
+
+function add_tomoe(){
+    eye.appendChild(tomoe_div.cloneNode(true));
+    get_tomoe();
+    set_tomoe_rotation();
+    if(tomoes.length > 0)
+        eye.classList.add('sharingan');
+}
+
+function remove_tomoe(){
+    eye.removeChild(tomoes[tomoes.length - 1]);
+    get_tomoe();
+    if(tomoes.length > 0)
+        set_tomoe_rotation();
+    else
+        eye.classList.remove('sharingan');
+}
+
+function set_tomoe_rotation(){
     for(let i = 0; i < tomoes.length; i++)
         tomoes[i].style.transform = `rotate(${i / tomoes.length}turn)`;
 }
-set_tomoe();
+
+set_tomoe_rotation();
